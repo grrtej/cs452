b87fe4ce5bff5acef5fa941638306e56cf1488bd
Gurtej Singh <grrtej@gmail.com>
2024-02-23 08:43:17 -0800

    use latest freertos

--- hellopico/main.cpp
+++ hellopico/main.cpp
@@ -1,19 +1,15 @@
-#include <FreeRTOS.h>
-#include <task.h>
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
 void timer_task(void *parameters)
 {
     while (true)
     {
-        // Convert ticks to seconds: divide by configTICK_RATE_HZ
-        // Convert seconds to ticks: multiply by configTICK_RATE_HZ
-        printf(
-            "Time since board powered on: %zu seconds\n",
-            xTaskGetTickCount() / configTICK_RATE_HZ
-        );
-        vTaskDelay(1 * configTICK_RATE_HZ);
+        printf("Time since board powered on: %u seconds\n",
+                pdTICKS_TO_MS(xTaskGetTickCount()) / 1000);
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
@@ -21,7 +17,7 @@ int main()
 {
     stdio_init_all();
 
-    xTaskCreate(timer_task, "Timer Task", 256, NULL, 1, NULL);
+    xTaskCreate(timer_task, "TIMER_TASK", 256, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while (true) {};
